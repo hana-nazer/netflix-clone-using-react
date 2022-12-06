4976995d7655dc61be39f0be2c32ce426bc0aa46
hana-nazer <hanapp9966@gmail.com>
2022-12-06 09:20:21 +0530

    header

---
+++ src/components/navBar/Banner/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className='banner'>
+     <div className='content'>
+
+     </div>
+    </div>
+  )
+}
+
+export default Banner
