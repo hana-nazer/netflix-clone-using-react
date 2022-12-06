18c486f4de241a0ab75900a0e3b5210efafc5eed
hana-nazer <hanapp9966@gmail.com>
2022-12-06 11:42:35 +0530

    structural design completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import NavBar from "./components/navBar/NavBar";
 import './App.css'
+import Banner from "./components/Banner/Banner";
+import RowPost from "./components/RowPost/RowPost";
 
 
 function App() {
   return (
     <div className="App">
     <NavBar></NavBar>
+    <Banner></Banner>
+    <RowPost></RowPost>
     </div>
   );
 }
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+    useEffect(() => {
+        
+    }, [])
+
+    return (
+        <div className='banner'>
+            <div className='content'>
+                <h1 className='title'>Movie Name</h1>
+                <div className='banner_buttons'>
+                    <button className='button'>Play</button>
+                    <button className='button'>My list</button>
+                </div>
+                <h1 className='description'>sgtfhjcusvf ny fuergjkljiv huireshkhv uibucyjinhien</h1>
+            </div>
+            <div className='fade_bottom'></div>
+        </div>
+    )
+}
+
+export default Banner
